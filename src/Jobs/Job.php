6028f48dca64b64d3409cabc63b969bc59c7d626
patryk <patzaj9711@gmail.com>
2023-05-13 10:03:01 +0200

    Extends Job interface

--- src/Jobs/Job.php
+++ src/Jobs/Job.php
@@ -4,7 +4,26 @@ declare(strict_types=1);
 
 namespace Scheduler\Jobs;
 
+use Cron\CronExpression;
+use DateTimeZone;
+use Scheduler\Exceptions\InvalidTimezoneException;
 
 interface Job {
-    function run(): void;
+
+    function run(\DateTimeImmutable $runDateTime): void;
+
+    /**
+     * setting timezone which are gona be used while running job
+     * 
+     * @throws InvalidTimezoneException
+     * @param string|DateTimeZone $timezone 
+     * @return void 
+     */
+    function setTimezone(string|\DateTimeZone $timezone): void;
+
+    function getTimezone(): null|\DateTimeZone;
+
+    function setCronExpression(string|CronExpression $cronExpression): void;
+
+    function getCronExpression(): CronExpression;
 }
